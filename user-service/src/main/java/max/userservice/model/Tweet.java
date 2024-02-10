e23ba76b50fe6795759970c34f98f646d181ffe9
Maxim ELISEYKIN <maxim.eliseykin@tipsport.cz>
2024-02-10 18:58:11 +0100

    Add methods to controller getAllTweets Add tweet service

--- user-service/src/main/java/max/userservice/model/Tweet.java
+++ user-service/src/main/java/max/userservice/model/Tweet.java
@@ -1,4 +1,35 @@
 package max.userservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tweets")
 public class Tweet {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "user_id")
+    @OneToOne(mappedBy = "id", fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private Long userId;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
 }
